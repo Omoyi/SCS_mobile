78cb917a63a9933876be5ffdc6e6b598dce51bef
lyndaflower <umurerwalynda@gmail.com>
2019-11-04 18:12:42 +0200

    add new activity

---
+++ app/src/main/java/com/myicecream/scs_mobile/groupActivity.java
@@ -0,0 +1,14 @@
+package com.myicecream.scs_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class groupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group);
+    }
+}
