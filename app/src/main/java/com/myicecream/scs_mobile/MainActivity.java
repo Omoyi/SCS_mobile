2a819a87fc333819c1518da2dba1ba5337b6d4b2
Omoyi <urakhel7@gmail.com>
2019-11-06 10:31:50 +0200

    create an activity

--- app/src/main/java/com/myicecream/scs_mobile/MainActivity.java
+++ app/src/main/java/com/myicecream/scs_mobile/MainActivity.java
@@ -2,14 +2,24 @@ package com.myicecream.scs_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIME_OUT );
     }
 }
