c43cbc04d3f258493a9bf5689cfff988a25a19f5
Omoyi <urakhel7@gmail.com>
2019-11-06 14:22:25 +0200

    create an activity

--- app/src/main/java/com/myicecream/scs_mobile/MainActivity.java
+++ app/src/main/java/com/myicecream/scs_mobile/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 4000;
+    static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
