24e97a5eb421ebc75207bfaa8978e6212d588ddd
Omoyi <urakhel7@gmail.com>
2019-11-06 22:09:42 +0200

    do a lot of stuff

--- app/src/main/java/com/myicecream/scs_mobile/MainActivity.java
+++ app/src/main/java/com/myicecream/scs_mobile/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
                 startActivity(intent);
                 finish();
             }
--- app/src/main/java/com/myicecream/scs_mobile/ProfileActivity.java
+++ app/src/main/java/com/myicecream/scs_mobile/ProfileActivity.java
@@ -2,7 +2,11 @@ package com.myicecream.scs_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    public void browser1(View view){
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto", "abc@gmail.com", null));
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "This is my subject text");
+        this.startActivity(Intent.createChooser(emailIntent, null));
+    }
+
+    public void browser2(View view){
+        String PhoneNum = "+250 787971925";
+        Intent callIntent = new Intent(Intent.ACTION_DIAL);
+        callIntent.setData(Uri.parse("tel:"+Uri.encode(PhoneNum.trim())));
+        callIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(callIntent);
+    }
+
+    public void browser4(View view){
+        Intent browser4Intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Omoyi"));
+        startActivity(browser4Intent);
     }
 }
