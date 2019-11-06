9a0ad387047310da1072579619bf27f5253093b7
Omoyi <urakhel7@gmail.com>
2019-11-06 22:59:12 +0200

    transition movement from an activity to another

--- app/src/main/java/com/myicecream/scs_mobile/MainActivity.java
+++ app/src/main/java/com/myicecream/scs_mobile/MainActivity.java
@@ -5,9 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-    static int SPLASH_TIME_OUT = 4000;
+    static int SPLASH_TIME_OUT = 3000;
+    private ImageView image, leaf;
+    private  LinearLayout animText, texth;
+    private  Animation frombottom;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +30,21 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         },SPLASH_TIME_OUT );
+
+        frombottom = AnimationUtils.loadAnimation(this, R.anim.anim1);
+
+
+        image = (ImageView) findViewById(R.id.image);
+        leaf = (ImageView) findViewById(R.id.leaf);
+        animText = (LinearLayout) findViewById(R.id.animText);
+        texth = (LinearLayout) findViewById(R.id.explore);
+
+
+        image.animate().translationY(-1900).setDuration(2000).setStartDelay(600);
+        leaf.animate().alpha(0).setDuration(2000).setStartDelay(900);
+        animText.animate().translationY(140).alpha(0).setDuration(1000).setStartDelay(600);
+
+        texth.startAnimation(frombottom);
+
     }
 }
