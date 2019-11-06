b18d48e4fdf8488fa740071dc507df247e47e8e4
Nyirate <rwakaemma34@gmail.com>
2019-11-06 10:15:55 +0200

    add new class

---
+++ app/src/main/java/com/myicecream/scs_mobile/Member1.java
@@ -0,0 +1,14 @@
+package com.myicecream.scs_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Member1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member1);
+    }
+}
