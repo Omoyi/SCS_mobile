ce095d35af59791358674fae94314f9eea85b045
Nyirate <rwakaemma34@gmail.com>
2019-11-06 15:47:24 +0200

    add all

--- app/src/main/java/com/myicecream/scs_mobile/Member1.java
+++ app/src/main/java/com/myicecream/scs_mobile/Member1.java
@@ -2,26 +2,39 @@ package com.myicecream.scs_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.GridView;
 
 public class Member1 extends AppCompatActivity {
 
-    GridView gridView;
-    private int [] picture = { R.drawable.image4,
-            R.drawable.image8,
-            R.drawable.image7,
-            R.drawable.image3,
-            R.drawable.iiiii,
-            R.drawable.image5,
-            R.drawable.image2,
-            R.drawable.back,
-            R.drawable.pic3,
-            R.drawable.pic4
-    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member1);
+
+
+    }
+    public void browser(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Imaraika"));
+        startActivity(browserIntent);
+    }
+
+    public void browser1(View view){
+        Intent browser1Intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Sangwa-C"));
+        startActivity(browser1Intent);
+    }
+
+    public void browser2(View view){
+        Intent browser2Intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/lyndaflower"));
+        startActivity(browser2Intent);
+    }
+
+    public void browser3(View view){
+        Intent browser3Intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Eliane-5"));
+        startActivity(browser3Intent);
     }
 }
