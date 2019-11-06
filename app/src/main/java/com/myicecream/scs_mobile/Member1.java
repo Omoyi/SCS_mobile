bf5f3c03c83dc915e5dbbed239bdb4ed12c83293
Nyirate <rwakaemma34@gmail.com>
2019-11-06 11:08:38 +0200

    add all

--- app/src/main/java/com/myicecream/scs_mobile/Member1.java
+++ app/src/main/java/com/myicecream/scs_mobile/Member1.java
@@ -3,9 +3,22 @@ package com.myicecream.scs_mobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class Member1 extends AppCompatActivity {
 
+    GridView gridView;
+    private int [] picture = { R.drawable.image4,
+            R.drawable.image8,
+            R.drawable.image7,
+            R.drawable.image3,
+            R.drawable.iiiii,
+            R.drawable.image5,
+            R.drawable.image2,
+            R.drawable.back,
+            R.drawable.pic3,
+            R.drawable.pic4
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
