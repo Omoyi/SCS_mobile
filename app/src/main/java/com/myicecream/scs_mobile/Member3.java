af749b72d7e25201f26efa4aad76e4b4747ed9c4
Nyirate <rwakaemma34@gmail.com>
2019-11-07 16:22:43 +0200

    add all

--- app/src/main/java/com/myicecream/scs_mobile/Member3.java
+++ app/src/main/java/com/myicecream/scs_mobile/Member3.java
@@ -1,40 +1,40 @@
-//package com.myicecream.scs_mobile;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import android.content.Intent;
-//import android.net.Uri;
-//import android.os.Bundle;
-//import android.view.View;
-//
-//
-//public class Member3 extends AppCompatActivity {
-//
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_member3);
-//
-//
-//    }
-//    public void brows1(View view){
-//        Intent brows1Intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Omoyi"));
-//        startActivity(brows1Intent);
-//    }
-//
-//    public void brows2(View view){
-//        Intent brows2Intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/claudinos"));
-//        startActivity(brows2Intent);
-//    }
-//
-//    public void brows3(View view){
-//        Intent brows3Intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Nyirate"));
-//        startActivity(brows3Intent);
-//    }
-//
-//    public void brows4(View view){
-//        Intent brows4Intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/GraceUmutesi"));
-//        startActivity(brows4Intent);
-//    }
-//}
+package com.myicecream.scs_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class Member3 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member3);
+
+
+    }
+    public void brows1(View view){
+        Intent brows1Intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Omoyi"));
+        startActivity(brows1Intent);
+    }
+
+    public void brows2(View view){
+        Intent brows2Intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/claudinos"));
+        startActivity(brows2Intent);
+    }
+
+    public void brows3(View view){
+        Intent brows3Intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Nyirate"));
+        startActivity(brows3Intent);
+    }
+
+    public void brows4(View view){
+        Intent brows4Intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/GraceUmutesi"));
+        startActivity(brows4Intent);
+    }
+}
--- app/src/main/java/com/myicecream/scs_mobile/Member4.java
+++ app/src/main/java/com/myicecream/scs_mobile/Member4.java
@@ -1,39 +1,39 @@
-package com.myicecream.scs_mobile;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
-
-
-public class Member4 extends AppCompatActivity {
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_member4);
-
-    }
-    public void browsi1(View view){
-        Intent browsi1Intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Mugengano18"));
-        startActivity(browsi1Intent);
-    }
-
-    public void browsi2(View view){
-        Intent browsi2Intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/RuzindanaWendyOrnella"));
-        startActivity(browsi2Intent);
-    }
-
-    public void browsi3(View view){
-        Intent browsi3Intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/niygomhonor"));
-        startActivity(browsi3Intent);
-    }
-
-    public void browsi4(View view){
-        Intent browsi4Intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/esthcarelle"));
-        startActivity(browsi4Intent);
-    }
-}
+//package com.myicecream.scs_mobile;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.content.Intent;
+//import android.net.Uri;
+//import android.os.Bundle;
+//import android.view.View;
+//
+//
+//public class Member4 extends AppCompatActivity {
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//       setContentView(R.layout.activity_member4);
+//
+//    }
+//    public void browsi1(View view){
+//        Intent browsi1Intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Mugengano18"));
+//        startActivity(browsi1Intent);
+//    }
+//
+//    public void browsi2(View view){
+//        Intent browsi2Intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/RuzindanaWendyOrnella"));
+//        startActivity(browsi2Intent);
+//    }
+//
+//    public void browsi3(View view){
+//        Intent browsi3Intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/niygomhonor"));
+//        startActivity(browsi3Intent);
+//    }
+//
+//    public void browsi4(View view){
+//        Intent browsi4Intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/esthcarelle"));
+//        startActivity(browsi4Intent);
+//    }
+//}
